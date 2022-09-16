a841f84655673f4e02e8b424e1c6d27f6d78bcde
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-16 13:39:15 -0700

    start popup build

---
+++ src/components/Popup/Popup.js
@@ -0,0 +1,16 @@
+import './Popup.scss';
+// import gif from '../../'
+
+function Popup() {
+    return (
+        <div className='popup-wrapper'>
+            <div className='popup'>
+                <h2 className='popup__header'>We got your back</h2>
+                <p className='popup__p'>Don’t settle for less when you can have this much fun. Simply select “Choices for everyone” or “Endless options for all” packages and enjoy limitless fun with your loved one.</p>
+
+            </div>
+        </div>
+    )
+}
+
+export default Popup;
