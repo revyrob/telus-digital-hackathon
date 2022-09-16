b1626aeb3f14f7c1f52f693afdbeb955c064b80e
revyrob <kayle.robson@gmail.com>
2022-09-16 00:55:29 -0700

    added a dummy header

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ThemeDrop from "./components/ThemeDrop/ThemeDrop";
 import ThemeSelection from "./components/ThemeSelection/ThemeSelection";
 import Categories from "./components/Categories/Categories";
+import DummyHeader from "./components/DummyHeader/DummyHeader";
 
 function App() {
   return (
     <>
+      <DummyHeader />
       <p>hello is the font working</p>
       return <ThemeDrop />
     </>
