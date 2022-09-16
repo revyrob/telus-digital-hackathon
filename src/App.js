58987cc5a44c76af6080e826bbae1a0e74792d53
Aya Seidemann <aya.seidemann@gmail.com>
2022-09-15 22:31:22 -0700

    add header and theme buttons

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ThemeDrop from "./components/ThemeDrop/ThemeDrop";
 import ThemeSelection from "./components/ThemeSelection/ThemeSelection";
+import Header from "./components/Header/Header";
 
 function App() {
   //return (
@@ -16,7 +17,12 @@ function App() {
  // );
 
   // return <ThemeDrop />;
-  return <ThemeSelection />
-}
+  return (
+    <div className="App">
+      <Header />
+      <ThemeSelection />
+    </div>
+
+  )}
 
 export default App;
