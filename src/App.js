942733bb7c993ee6841892834d15b433afd6bf36
revyrob <kayle.robson@gmail.com>
2022-09-16 13:22:02 -0700

    merged all files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
 import "./App.scss";
-
 import ThemeSelection from "./components/ThemeSelection/ThemeSelection";
-
 import Header from "./components/Header/Header";
 import PackageSelector from "./components/PackageSelector/PackageSelector";
-import ThemeDrop from "./components/ThemeDrop/ThemeDrop";
 
 function App() {
   return (
